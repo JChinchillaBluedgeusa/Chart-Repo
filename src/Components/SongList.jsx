add657574a0b266a037237526a0ca7069452a8e8
jchinchilla91 <joshuachinchilla91@gmail.com>
2024-12-21 16:39:34 -0800

    remove quotations when sorting

--- src/Components/SongList.jsx
+++ src/Components/SongList.jsx
@@ -3,8 +3,8 @@ import songJSON from '../assets/songs.json'
 
 function SongList() {
 	const [list, setList] = useState(songJSON.sort((a,b) => {
-		const artistA = a.Artist.toUpperCase();
-		const artistB = b.Artist.toUpperCase();
+		const artistA = a.Artist.toUpperCase().replaceAll(/"|'/g, '');
+		const artistB = b.Artist.toUpperCase().replaceAll(/"|'/g, '');
 
 		if (artistA < artistB) {
 			return -1;
@@ -17,19 +17,19 @@ function SongList() {
 		return 0;
 	}));
 
-	const songs = list.map((song) => {
+	const songs = list.map((song, i) => {
 		return (
-			<>
+			<div className='song' key={`${i}-${song.songlength}-${song.Charter}`}>
 				<h3>{song.Artist}</h3>
 				<p>{song.Name}</p>
-			</>
+			</div>
 		)
 	})
 
 	return (
-		<>
+		<div className='songs-wrapper'>
 			{songs}
-		</>
+		</div>
 	)
 }
 
