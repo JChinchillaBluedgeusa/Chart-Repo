1246b665a010e7139e06800b9d6d416444c39197
Josh <joshua@bluedgeusa.com>
2024-12-21 21:31:23 -0800

    search working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import SongList from './Components/SongList'
+import { Container } from '@mui/material';
 
 function App() {
 	return (
-		<SongList />
+		<Container fixed>
+			<SongList />
+		</Container>
 	)
 }
 
--- src/Components/SongList.jsx
+++ src/Components/SongList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import songJSON from '../assets/songs.json'
+import { Box, TextField } from '@mui/material';
 
 function SongList() {
 	const [list, setList] = useState(songJSON.sort((a,b) => {
@@ -16,20 +17,38 @@ function SongList() {
 
 		return 0;
 	}));
-
-	const songs = list.map((song, i) => {
-		return (
-			<div className='song' key={`${i}-${song.songlength}-${song.Charter}`}>
-				<h3>{song.Artist}</h3>
-				<p>{song.Name}</p>
-			</div>
-		)
-	})
+	const [searchValue, setSearchValue] = useState('');
 
 	return (
-		<div className='songs-wrapper'>
-			{songs}
-		</div>
+		<Box
+			sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+		>
+			<TextField
+				id="standard-basic"
+				label="Search..."
+				value={searchValue}
+				placeholder='Search...'
+				onChange={(e) => {
+					console.log('e', e.target.value)
+					setSearchValue(e.target.value);
+					
+				}}
+				variant='standard'
+			/>
+			<div className='songs-wrapper'>
+				{list.map((song, i) => {
+					if ((song.Artist.toLowerCase().includes(searchValue.toLowerCase()) || song.Name.toLowerCase().includes(searchValue.toLowerCase())) || !searchValue)
+					return (
+						<div className='song' key={`${i}-${song.songlength}-${song.Charter}`}>
+							<h3>{song.Artist}</h3>
+							<p>{song.Name}</p>
+						</div>
+						)
+					})
+				}
+			</div>
+		</Box>
+		
 	)
 }
 
